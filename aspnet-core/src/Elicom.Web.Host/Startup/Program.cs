fccb66a752b402ebaf369a7c32fcd2a88f64d749
adeelpris <noshahidevelopersinc@gmail.com>
2026-02-06 16:17:18 +0500

    downgrade to .net

--- aspnet-core/src/Elicom.Web.Host/Startup/Program.cs
+++ aspnet-core/src/Elicom.Web.Host/Startup/Program.cs
@@ -6,8 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Elicom.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System;
-using Microsoft.Extensions.Configuration;
 
 namespace Elicom.Web.Host.Startup
 {
@@ -15,43 +13,23 @@ namespace Elicom.Web.Host.Startup
     {
         public static void Main(string[] args)
         {
-            System.Console.WriteLine(">>> Application starting...");
-            var envName = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            System.Console.WriteLine($">>> Environment: {envName}");
-            System.Console.WriteLine($">>> Current Directory: {System.IO.Directory.GetCurrentDirectory()}");
+            var host = CreateHostBuilder(args).Build();
 
-            try 
+            using (var scope = host.Services.CreateScope())
             {
-                var host = CreateHostBuilder(args).Build();
-                var config = host.Services.GetRequiredService<Microsoft.Extensions.Configuration.IConfiguration>();
-                var connString = config.GetConnectionString("Default");
-                System.Console.WriteLine($">>> Connection String Found: {!string.IsNullOrEmpty(connString)}");
-                if (!string.IsNullOrEmpty(connString)) {
-                    System.Console.WriteLine($">>> Connection String Length: {connString.Length}");
-                    System.Console.WriteLine($">>> Connection String Start: {connString.Substring(0, Math.Min(20, connString.Length))}...");
-                }
-                
-                using (var scope = host.Services.CreateScope())
+                try
                 {
-                    try
-                    {
-                        // var context = scope.ServiceProvider.GetRequiredService<ElicomDbContext>();
-                        // context.Database.Migrate();
-                    }
-                    catch (System.Exception ex)
-                    {
-                        var logger = scope.ServiceProvider.GetRequiredService<Microsoft.Extensions.Logging.ILogger<Program>>();
-                        logger.LogCritical(ex, "An error occurred while migrating the database.");
-                    }
+                    // var context = scope.ServiceProvider.GetRequiredService<ElicomDbContext>();
+                    // context.Database.Migrate();
                 }
+                catch (System.Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<Microsoft.Extensions.Logging.ILogger<Program>>();
+                    logger.LogCritical(ex, "An error occurred while migrating the database.");
+                }
+            }
 
-                host.Run();
-            }
-            catch (System.Exception ex)
-            {
-                System.Console.WriteLine($">>> FATAL ERROR IN MAIN: {ex}");
-                throw;
-            }
+            host.Run();
         }
 
         internal static IHostBuilder CreateHostBuilder(string[] args) =>
