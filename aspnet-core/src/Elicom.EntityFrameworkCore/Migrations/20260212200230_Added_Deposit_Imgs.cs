a4c91cb136ee57debdeb14900419cfb8de85b438
adeelpris <noshahidevelopersinc@gmail.com>
2026-02-13 09:57:29 +0500

    Updated Withdraw

---
+++ aspnet-core/src/Elicom.EntityFrameworkCore/Migrations/20260212200230_Added_Deposit_Imgs.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elicom.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Deposit_Imgs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "LocalAmount",
+                table: "WithdrawRequests",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocalCurrency",
+                table: "WithdrawRequests",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentProof",
+                table: "WithdrawRequests",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocalAmount",
+                table: "WithdrawRequests");
+
+            migrationBuilder.DropColumn(
+                name: "LocalCurrency",
+                table: "WithdrawRequests");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentProof",
+                table: "WithdrawRequests");
+        }
+    }
+}
