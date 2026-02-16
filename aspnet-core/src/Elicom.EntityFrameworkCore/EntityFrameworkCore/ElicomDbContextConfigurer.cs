089b4abcee65c38a2984c8cab47ab65acce14b21
noshahi-devs <noshahidevelopersinc@gmail.com>
2026-02-17 01:07:51 +0500

    Added debug logs to ElicomDbContextConfigurer to verify deployment status. Confirming that EnableRetryOnFailure is definitely removed.

--- aspnet-core/src/Elicom.EntityFrameworkCore/EntityFrameworkCore/ElicomDbContextConfigurer.cs
+++ aspnet-core/src/Elicom.EntityFrameworkCore/EntityFrameworkCore/ElicomDbContextConfigurer.cs
@@ -8,12 +8,14 @@ public static class ElicomDbContextConfigurer
 {
     public static void Configure(DbContextOptionsBuilder<ElicomDbContext> builder, string connectionString)
     {
+        Console.WriteLine("[DB-CONFIG] Configuring DbContext (No Retries)");
         builder.UseSqlServer(connectionString, options => options.CommandTimeout(180))
                .ConfigureWarnings(w => w.Throw(SqlServerEventId.SavepointsDisabledBecauseOfMARS));
     }
 
     public static void Configure(DbContextOptionsBuilder<ElicomDbContext> builder, DbConnection connection)
     {
+        Console.WriteLine("[DB-CONFIG] Configuring DbContext with Connection (No Retries)");
         builder.UseSqlServer(connection, options => options.CommandTimeout(180))
                .ConfigureWarnings(w => w.Throw(SqlServerEventId.SavepointsDisabledBecauseOfMARS));
     }
