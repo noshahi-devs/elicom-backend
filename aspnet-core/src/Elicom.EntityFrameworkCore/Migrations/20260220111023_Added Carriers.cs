001544a6574feb2c1d7ed6d294a879c8b0c2cd48
noshahi-devs <noshahidevelopersinc@gmail.com>
2026-02-20 16:11:19 +0500

    Added Migrations for Order failure

---
+++ aspnet-core/src/Elicom.EntityFrameworkCore/Migrations/20260220111023_Added Carriers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elicom.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCarriers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CarrierId",
+                table: "SupplierOrders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ShipmentDate",
+                table: "SupplierOrders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrackingCode",
+                table: "SupplierOrders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CarrierId",
+                table: "SupplierOrders");
+
+            migrationBuilder.DropColumn(
+                name: "ShipmentDate",
+                table: "SupplierOrders");
+
+            migrationBuilder.DropColumn(
+                name: "TrackingCode",
+                table: "SupplierOrders");
+        }
+    }
+}
